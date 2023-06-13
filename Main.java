982180ba05c869ff0a97a513ef1166e4087e91e5
DanielPizon121 <135995570+DanielPizon121@users.noreply.github.com>
2023-06-13 20:55:37 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -35,8 +35,23 @@ public class Main {
             System.out.println();
         }
     }
+    import java.util.Random;
+
+class Main {
+  public static void main(String[] args) {
+int [][] array=new int[10][10];
+    Random rand = new Random();
+    for(int i=0;i<10;i++){
+      for (int j = 0; j<10;j++){
+        array[i][j]=rand.nextInt(101)-50;
+        System.out.print(array[i][j]+ "\t");
+      }
+      System.out.println();
+    }
+  }
+}
 }
 
 
 
-napisz program aiuhdsiusabfiasfas
+
